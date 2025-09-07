3f31fb4680a3083207826bc69fce16165d941189
trashh <egor.perri2006@gmail.com>
2025-09-07 15:50:29 +0300

    added logger class

--- source/Logger.hpp
+++ source/Logger.hpp
@@ -6,6 +6,8 @@
 #define LOGGER_HPP
 
 #include <cstddef>
+#include <iostream>
+#include <utility>
 
 
 namespace pentester {
@@ -23,7 +25,57 @@ namespace pentester {
     NTTPString(const char (&s)[N]) -> NTTPString<N - 1>;
 
     class Logger {
+    public:
 
+        template<NTTPString Str, typename ...Args>
+        void log(Args&& ...args) {
+            static_assert(countAmpr<Str>() == sizeof...(args));
+
+            log_impl<Str>(0, std::forward<Args>(args)...);
+        }
+    private:
+
+        template<NTTPString Str>
+        void log_impl(std::size_t index) {
+            while (Str.data[index]) {
+                if (Str.data[index] == '&') [[unlikely]] {
+                    index += 1;
+                }
+                std::cout << Str.data[index];
+                index += 1;
+            }
+        }
+
+        template<NTTPString Str, typename T, typename ...Args>
+        void log_impl(std::size_t index, T&& value, Args&& ...args) {
+            while (Str.data[index]) {
+                if (Str[index] == '&') {
+                    if (index + 1 < Str.size && Str.data[index + 1] == '&') [[unlikely]] {
+                        index += 1;
+                    } else {
+                        std::cout << value << '\n';
+                        log_impl(index + 1, std::forward<Args>(args)...);
+                        return;
+                    }
+                }
+                std::cout << Str.data[index] << '\n';
+
+                index += 1;
+            }
+        }
+
+        template<NTTPString str>
+        consteval std::size_t countAmpr() {
+            std::size_t result = 0;
+            for (std::size_t i = 0; i < str.size; i++) {
+                if (str[i] == '&') {
+                    if (i >= str.size || str[i + 1] != '&') {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
     };
 }
 
